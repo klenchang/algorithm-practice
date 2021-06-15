0557fc6465632dc63dbbba0254d59aa73da3837e
Klen <klenchang0313@gmail.com>
2021-06-15 10:41:20 +0800

    implement Prime and composite numbers/Peaks

---
+++ AlgorithmPractice/AlgorithmPracticeTests/Codility/Lessons/PrimeAndCompositeNumbers/PeaksTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmPractice.Codility.Lessons.PrimeAndCompositeNumbers.Tests
+{
+    [TestClass()]
+    public class PeaksTests
+    {
+        [TestMethod()]
+        public void SolutionTest()
+        {
+            var array = new[] { 1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2 };
+            var actual = Peaks.Solution(array);
+            var expected = 3;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void SolutionTest_2()
+        {
+            var array = new[] { 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2 };
+            var actual = Peaks.Solution(array);
+            var expected = 4;
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
