93f52470e58366a6417fd649f70c8b27acf3e6a9
Klen <klenchang0313@gmail.com>
2021-06-18 10:49:31 +0800

    implement Caterpillar Method/CountDistinctSlices

---
+++ AlgorithmPractice/AlgorithmPracticeTests/Codility/Lessons/CaterpillarMethod/CountDistinctSlicesTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmPractice.Codility.Lessons.CaterpillarMethod.Tests
+{
+    [TestClass()]
+    public class CountDistinctSlicesTests
+    {
+        [TestMethod()]
+        public void SolutionTest()
+        {
+            var M = 6;
+            var A = new[] { 3, 4, 5, 5, 2 };
+            var actual = CountDistinctSlices.Solution(M, A);
+            var expected = 9;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void SolutionTest_1()
+        {
+            var M = 6;
+            var A = new[] { 3, 4, 5, 5, 5 };
+            var actual = CountDistinctSlices.Solution(M, A);
+            var expected = 8;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void SolutionTest_2()
+        {
+            var M = 6;
+            var A = new[] { 3, 3, 3, 4, 5, 5, 5 };
+            var actual = CountDistinctSlices.Solution(M, A);
+            var expected = 10;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void SolutionTest_3()
+        {
+            var M = 6;
+            var A = new[] { 3, 4, 5, 2, 2, 5, 2 };
+            //(3, 4, 5, 2) + (2, 5) + (5, 2) - duplicate 5(index is A.Length - 2)
+            var actual = CountDistinctSlices.Solution(M, A);
+            var expected = 15;
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
