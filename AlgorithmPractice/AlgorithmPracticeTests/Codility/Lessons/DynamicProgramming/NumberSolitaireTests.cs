63b21578a25f5b1cfb098cbe7f7f33fe88e729a1
Klen <klenchang0313@gmail.com>
2021-06-19 00:15:42 +0800

    implement Dynamic Programming/NumberSolitaire

---
+++ AlgorithmPractice/AlgorithmPracticeTests/Codility/Lessons/DynamicProgramming/NumberSolitaireTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmPractice.Codility.Lessons.DynamicProgramming.Tests
+{
+    [TestClass()]
+    public class NumberSolitaireTests
+    {
+        [TestMethod()]
+        public void SolutionTest()
+        {
+            var A = new[] { 1, -2, 0, 9, -1, -2 };
+            var actual = NumberSolitaire.Solution(A);
+            var expected = 8;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void SolutionTest1()
+        {
+            var A = new[] { 1, -1, -2, -3, -4, -5, -6, -7, -9, -9, -9, -9 };
+            var actual = NumberSolitaire.Solution(A);
+            var expected = -13;
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
