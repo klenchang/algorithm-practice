c244278f0adc9c3973dd2b084faf4cdfec306f84
Klen <klenchang0313@gmail.com>
2021-06-18 15:40:22 +0800

    implement Greedy Algorithms

---
+++ AlgorithmPractice/AlgorithmPracticeTests/Codility/Lessons/GreedyAlgorithms/MaxNonoverlappingSegmentsTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmPractice.Codility.Lessons.GreedyAlgorithms.Tests
+{
+    [TestClass()]
+    public class MaxNonoverlappingSegmentsTests
+    {
+        [TestMethod()]
+        public void SolutionTest()
+        {
+            var A = new[] { 1, 3, 7, 9, 9 };
+            var B = new[] { 5, 6, 8, 9, 10 };
+            var actual = MaxNonoverlappingSegments.Solution(A, B);
+            var expected = 3;
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ AlgorithmPractice/AlgorithmPracticeTests/Codility/Lessons/GreedyAlgorithms/TieRopesTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmPractice.Codility.Lessons.GreedyAlgorithms.Tests
+{
+    [TestClass()]
+    public class TieRopesTests
+    {
+        [TestMethod()]
+        public void SolutionTest()
+        {
+            var K = 4;
+            var A = new[] { 1, 2, 3, 4, 1, 1, 3 };
+            var actual = TieRopes.Solution(K, A);
+            var expected = 3;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void SolutionTest1()
+        {
+            var K = 5;
+            var A = new[] { 1, 2, 3, 4, 1, 1, 3 };
+            var actual = TieRopes.Solution(K, A);
+            var expected = 2;
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
