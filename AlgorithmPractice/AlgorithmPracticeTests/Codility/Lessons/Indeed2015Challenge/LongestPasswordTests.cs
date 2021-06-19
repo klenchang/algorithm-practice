ea6849b40b55e30fd7acf1d411def1df95a9cbb3
Klen <klenchang0313@gmail.com>
2021-06-19 08:42:03 +0800

    implement Tasks from Indeed Prime 2015 challenge/LongestPassword

---
+++ AlgorithmPractice/AlgorithmPracticeTests/Codility/Lessons/Indeed2015Challenge/LongestPasswordTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmPractice.Codility.Lessons.Indeed2015Challenge.Tests
+{
+    [TestClass()]
+    public class LongestPasswordTests
+    {
+        [TestMethod()]
+        public void SolutionTest()
+        {
+            var S = "test 5 a0A pass007 ?xy1";
+            var actual = LongestPassword.Solution(S);
+            var expected = 7;
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
