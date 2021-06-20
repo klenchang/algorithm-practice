41daf78ebcaccf9c6bcc75b911fd260aa91ad775
Klen <klenchang0313@gmail.com>
2021-06-20 17:25:00 +0800

    implement Tasks from Indeed Prime 2016 College Coders challenge/TennisTournament

---
+++ AlgorithmPractice/AlgorithmPracticeTests/Codility/Lessons/Indeed2016CollegeCodersChallenge/TennisTournamentTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmPractice.Codility.Lessons.Indeed2016CollegeCodersChallenge.Tests
+{
+    [TestClass()]
+    public class TennisTournamentTests
+    {
+        [TestMethod()]
+        public void SolutionTest()
+        {
+            var P = 10;
+            var C = 3;
+            var actual = TennisTournament.Solution(P, C);
+            var expected = 3;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SolutionTest1()
+        {
+            var P = 5;
+            var C = 3;
+            var actual = TennisTournament.Solution(P, C);
+            var expected = 2;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
