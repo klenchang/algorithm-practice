3aa0c5d3a93d92db3a790ab7de3c13386e41e453
Klen <klenchang0313@gmail.com>
2021-06-28 10:04:49 +0800

    implement some dynamic program topic from hackerrank

---
+++ AlgorithmPractice/AlgorithmPractice/Hackerrank/Interview/DynamicProgram/Abbreviation.cs
@@ -0,0 +1,88 @@
+﻿namespace AlgorithmPractice.Hackerrank.Interview.DynamicProgram
+{
+    public class Abbreviation
+    {
+        #region topic
+        /*
+        You can perform the following operations on the string, a:
+            Capitalize zero or more of a's lowercase letters.
+            Delete all of the remaining lowercase letters in a. 
+
+        Given two strings, a and b, determine if it's possible to make a equal to b as described. 
+        If so, print YES on a new line. Otherwise, print NO.
+
+        Example.
+        a = AbcDE, b = ABDE => YES
+        a = AbcDE, b = AFDE => NO
+        a = AbBcDE, b = ABCDE => YES
+
+        Constraints
+        1 <= q <= 10
+        1 <= a.Length, b.Length <= 1000
+        a consists with [a-zA-Z]
+        b consists with [A-Z]
+         */
+        #endregion
+
+        /// <summary>
+        /// refer to geraldlow from https://www.hackerrank.com/challenges/abbr/forum/comments/484833
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static string Solution(string a, string b)
+        {
+            var isValid = new bool[a.Length + 1, b.Length + 1];
+            // initializing the first raw to all false; ie. if b is
+            // not empty, isValid will always be false
+            isValid[0, 0] = true;
+            // array initialization - if a is non-empty but b is empty,
+            // then isValid == true iff remaining(a) != contain uppercase
+            bool containsUppercase = false;
+            for (int k = 1; k <= a.Length; k++)
+            {
+                int i = k - 1;
+                // if the pointer at string a is uppercase
+                if (a[i] <= 'Z' && a[i] >= 'A' || containsUppercase)
+                {
+                    containsUppercase = true;
+                    isValid[k, 0] = false;
+                }
+                else isValid[k, 0] = true;
+            }
+            // tabulation from start of string
+            for (int k = 1; k <= a.Length; k++)
+            {
+                for (int l = 1; l <= b.Length; l++)
+                {
+                    int i = k - 1; int j = l - 1;
+                    // when the characters are equal, set = previous character bool.
+                    if (a[i] == b[j])
+                    {
+                        isValid[k, l] = isValid[k - 1, l - 1];
+                        continue;
+                    }
+                    // elif uppercase a == b, set = prev character bool. or just eat a.
+                    else if ((int)a[i] - 32 == (int)b[j])
+                    {
+                        isValid[k, l] = isValid[k - 1, l - 1] || isValid[k - 1, l];
+                        continue;
+                    }
+                    // elif a is uppercase and no more b, or uppercase a is not b, then false
+                    else if (a[i] <= 90 && a[i] >= 65)
+                    {
+                        isValid[k, l] = false;
+                        continue;
+                    }
+                    //else just eat a
+                    else
+                    {
+                        isValid[k, l] = isValid[k - 1, l];
+                        continue;
+                    }
+                }
+            }
+            return isValid[a.Length, b.Length] ? "YES" : "NO";
+        }
+    }
+}
---
+++ AlgorithmPractice/AlgorithmPractice/Hackerrank/Interview/DynamicProgram/MaxArraySum.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace AlgorithmPractice.Hackerrank.Interview.DynamicProgram
+{
+    public class MaxArraySum
+    {
+        #region topic
+        /*
+        Given an array of integers, find the subset of non-adjacent elements with the maximum sum. 
+        Calculate the sum of that subset. 
+        It is possible that the maximum sum is 0, the case when all elements are negative. 
+
+        Example.
+        arr = [-2, 1, 3, -4, 5]
+        The following subsets with more than 1 element exist. 
+        These exclude the empty subset and single element subsets which are also valid.
+
+        Subset      Sum
+        [-2, 3, 5]   6
+        [-2, 3]      1
+        [-2, -4]    -6
+        [-2, 5]      3
+        [1, -4]     -3
+        [1, 5]       6
+        [3, 5]       8
+        The maximum subset sum is 8. Note that any individual element is a subset as well.
+
+        arr = [-2, -3, -1] => 0
+
+        Constraints
+        1 <= n <= 10 ** 5
+        -10 ** 4 <= arr[i] <= 10 ** 4
+         */
+        #endregion
+
+        public static int MaxSubsetSum(int[] arr)
+        {
+            if (arr.Length == 0)
+                return 0;
+            arr[0] = Math.Max(0, arr[0]);
+            if (arr.Length == 1)
+                return arr[0];
+            arr[1] = Math.Max(arr[0], arr[1]);
+            for (int i = 2; i < arr.Length; i++)
+                arr[i] = Math.Max(arr[i - 1], arr[i] + arr[i - 2]);
+            
+            return arr[arr.Length - 1];
+        }
+    }
+}
