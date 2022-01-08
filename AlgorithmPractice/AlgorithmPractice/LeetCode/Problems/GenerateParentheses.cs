0bea0f1bb1b328982f2cf9f2a5b7235a9456549f
Klen <klenchang0313@gmail.com>
2022-01-08 13:49:09 +0800

    recursive practice - generate all combinations of well-formed parentheses

---
+++ AlgorithmPractice/AlgorithmPractice/LeetCode/Problems/GenerateParentheses.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace AlgorithmPractice.LeetCode.Problems
+{
+    public class GenerateParentheses
+    {
+        #region topic
+        /*
+         give a number n, write a function to generate all combinations of well-formed parentheses.
+         ex. n = 3, output = ["((()))","(()())","(())()","()(())","()()()"]
+             n = 1, output = ["()"]
+         */
+        #endregion
+
+        public static IList<string> GenerateParenthesis(int n)
+        {
+            var list = new List<string>();
+            Recursive(n, n, "", list);
+            return list;
+        }
+
+        private static void Recursive(int leftCount, int rightCount, string tempResult, List<string> list)
+        {
+            if (leftCount == 0 && rightCount == 0)
+                list.Add(tempResult);
+            if (leftCount > 0)
+                Recursive(leftCount - 1, rightCount, tempResult + "(", list);
+            if (rightCount > leftCount)
+                Recursive(leftCount, rightCount - 1, tempResult + ")", list);
+        }
+    }
+}
---
+++ AlgorithmPractice/AlgorithmPracticeTests/LeetCode/Problems/GenerateParenthesesTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmPractice.LeetCode.Problems.Tests
+{
+    [TestClass()]
+    public class GenerateParenthesesTests
+    {
+        [TestMethod()]
+        public void GenerateParenthesisWithNEqualOne()
+        {
+            var n = 1;
+            var actual = GenerateParentheses.GenerateParenthesis(n).ToList();
+            var expected = new List<string> { "()" };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GenerateParenthesisWithNEqualThree()
+        {
+            var n = 3;
+            var actual = GenerateParentheses.GenerateParenthesis(n).ToList();
+            var expected = new List<string> { "((()))", "(()())", "(())()", "()(())", "()()()" };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
